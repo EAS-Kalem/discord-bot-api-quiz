68fdbfdbb463261df06f1127af31585c272e4d93
admin <kalem@enterpriseautomation.co.uk>
2022-04-15 10:57:04 +0100

    add function tested and working as intended, search by name to retrieve scores tested and working as intended, create search by topic function for questions to dispay only one type of questions e.g. 'Node questions' and tested

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const res = require("express/lib/response");
 let router = express.Router();
 let quizRepo = require("./repos/quizRepo")
-
 const app = express();
 app.use(express.json());
 
+
+//DESCRIPTION: Get all questions
+//USE: GET http://localhost:3000/api
+//TODO: **Admin only functionality to be added**
 router.get('/', function (req, res, next) {
     quizRepo.get(function (data) {
         res.status(200).json({
@@ -19,41 +22,19 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.get('/search/:id', function (req, res, next) {
-    quizRepo.getById(req.params.id, function (data) {
-        if (data) {
-            res.status(200).json({
-                "status": 200,
-                "statusText": "OK",
-                "message": "individual score retrived.",
-                "data": data
-            });
-        } else {
-            res.status(404).json({
-                "status": 404,
-                "statusText": "Not Found",
-                "message": "The score for '" + req.params.id + " ' could not be found.",
-                "error": {
-                    "code": "NOT_FOUND",
-                    "message": "The score for '" + req.params.id + " ' could not be found.",
-                }
-            });
-        }
-    }, function (err) {
-        next(err);
-    });
-});
 
-
-router.get('/search/:topic', function (req, res, next) {
+//DESCRIPTION: Search score and total answered questions for the user
+//USE: GET http://localhost:3000/api/searchscore/?name=kalem
+//TODO: **Categorise based on percentage answered right (Noob, Student, Wise & Veteran)**
+router.get('/searchscore', function (req, res, next) {
     let searchObject = {
-        "topic": req.query.name
+        "name": req.query.name
     };
-    quizRepo.search(searchObject, function (data) {
+    quizRepo.searchScores(searchObject, function (data) {
         res.status(200).json({
             "status": 200,
             "statusText": "OK",
-            "message": "Search topic question retrived.",
+            "message": "Individual score retrived.",
             "data": data
         });
 
@@ -61,6 +42,32 @@ router.get('/search/:topic', function (req, res, next) {
         next(err);
     });
 });
+
+
+// DESCRIPTION: Search questions.json for questions of a certian topic e.g. "node", "git"
+// USE: GET http://localhost:3000/api/searchtopic/?topic=node
+// TODO: **Only display 5 questions to be added***
+router.get('/searchtopic', function (req, res, next) {
+    let searchObject = {
+        "topic": req.query.topic
+    };
+    quizRepo.searchTopic(searchObject, function (data) {
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "A " + req.query.topic + " quiz was created",
+            "data": data
+        });
+
+    }, function (err) {
+        next(err);
+    });
+});
+
+
+// DESCRIPTION: Add questions to questions.json 
+// USE: POST http://localhost:3000/api
+// TODO: **Skill tag only functionality to be added (node questions can be added by users with the node tag)***
 router.post('/', function (req, res, next) {
     quizRepo.insert(req.body, function (data) {
         res.status(201).json({
@@ -74,9 +81,9 @@ router.post('/', function (req, res, next) {
     })
 });
 
-app.use('/api/', router); 
 
+app.use('/api/', router);
 app.listen(3000, () => {
-    console.log('Node server is running on port 6000');
+    console.log('Node server is running on port 3000');
 })
 
